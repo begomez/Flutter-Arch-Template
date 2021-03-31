fbd54b37a976078d3314bd6a56e2fdcf39643410
Bernat Gómez <bernat.gomez@gmail.com>
2021-03-31 18:55:28 +0200

    Add logger

--- lib/presentation/utils/AppLogger.dart
+++ lib/presentation/utils/AppLogger.dart
@@ -1,11 +1,11 @@
 abstract class AppLogger {
   static const String TAG = "AppLogger";
 
-  void i({String tag = TAG, String msg = ""}) {
+  static void i({String tag = TAG, String msg = ""}) {
     print("$tag $msg");
   }
 
-  void e({String tag = TAG, String msg = "", Exception error}) {
+  static void e({String tag = TAG, String msg = "", Exception error}) {
     print("$tag $msg");
   }
 }

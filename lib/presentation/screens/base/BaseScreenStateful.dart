eb1f88fac6b97ebd984b34229a880c39561b6cae
Bernat Gómez <bernat.gomez@gmail.com>
2021-03-31 16:19:46 +0200

    Add empty base class

---
+++ lib/presentation/screens/base/BaseScreenStateful.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//TODO: complete
+
+/**
+ * 
+ */
+class BaseScreenStateful extends StatefulWidget {
+  @override
+  _BaseScreenStatefulState createState() => _BaseScreenStatefulState();
+}
+
+/**
+ * 
+ */
+class _BaseScreenStatefulState extends State<BaseScreenStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

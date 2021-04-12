90e0eb335175bce2685c135cfebadcbe07a9f441
Bernat Gómez <bernat.gomez@gmail.com>
2021-04-12 12:25:54 +0200

    Add DTOs on domain layer

---
+++ lib/domain/dto/LoginDTO.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/domain/dto/core/BaseDTO.dart';
+
+class LoginDTO extends BaseDTO {
+  final String user;
+  final String pass;
+
+  const LoginDTO({@required this.user, @required this.pass}) : super();
+}
---
+++ lib/domain/dto/core/BaseDTO.dart
@@ -0,0 +1,3 @@
+class BaseDTO {
+  const BaseDTO();
+}

b9a5057ffb7f936fd79d4b27b77f6add9bfb5acb
Bernat Gómez <bernat.gomez@gmail.com>
2021-04-12 12:20:57 +0200

    Add common models

---
+++ lib/common/models/BooleanWrapper.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_template/common/models/BaseModel.dart';
+
+class BooleanWrapper extends BaseModel {
+  final bool flag;
+
+  const BooleanWrapper({this.flag = false}) : super();
+}
---
+++ lib/common/models/UserSessionModel.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/common/models/BaseModel.dart';
+
+class UserSessionModel extends BaseModel {
+  final String name;
+  final String mail;
+  final String token;
+
+  factory UserSessionModel.invalid() =>
+      UserSessionModel(name: "", mail: "", token: "");
+
+  const UserSessionModel(
+      {@required this.name, @required this.mail, @required this.token})
+      : assert(name != null),
+        assert(mail != null),
+        assert(token != null),
+        super();
+
+  bool validate() =>
+      this.name.isNotEmpty && this.mail.isNotEmpty && this.token.isNotEmpty;
+}
